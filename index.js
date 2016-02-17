b00ddd211da7cac81ef7a5142f4ccd433a7ee6b9
xuexb <fe.xiaowu@gmail.com>
2016-02-17 11:22:01 +0800

    test

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var Apijs = require('apijs');
 
 var app = new Apijs({
     cachePath: './cache/',
-    port: 8001
+    port: 8001,
+    global: {
+        nologin: {
+            errno: 10086,
+            errmsg: '没登录'
+        }
+    }
 });
 
 app.run();
